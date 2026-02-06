bc1721d33c6f6824b8ffccb9b56c344cec913838
Ambrish-M <ambrish2706@gmail.com>
2026-02-06 13:09:56 +0530

    Fix secure cookie issue on Render

--- server.js
+++ server.js
@@ -3,23 +3,29 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/auth.route.js";
-import postRoutes from "./routes/post.route.js"
+import postRoutes from "./routes/post.route.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
-const app =express();
+const app = express();
 
-app.use(cookieParser())
-app.use(cors({
-  origin: ["http://localhost:5173","https://mini-social-post-application.netlify.app"], // frontend URL 
-  credentials: true
-}));
+app.set("trust proxy", 1);
+
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://mini-social-post-application.netlify.app",
+    ], // frontend URL
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
-app.use("/api/auth",authRoutes);
-app.use("/api/posts",postRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
 
-
-app.listen(process.env.PORT,()=>{
-    console.log(`server is running on http://localhost:${process.env.PORT}`)
-})
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on http://localhost:${process.env.PORT}`);
+});
 connectDB();
