9cbfe5922ae4a9b5b5c2aeeab5120764cb18998b
Ambrish-M <ambrish2706@gmail.com>
2026-02-06 14:33:02 +0530

    bug fixing

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://mini-social-posts-application.netlify.app",
+      "https://mini-social-post-applications.netlify.app",
     ],
     credentials: true,
   }),
